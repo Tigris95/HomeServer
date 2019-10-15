51432de11d26ab1c17e085226093386a57a9626a
Tigris95 <tigrisgithub95@gmail.com>
2019-10-15 13:34:19 +0200

    Add DB connect using mongoose
    ...

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const PORT = process.env.PORT || 3000
+
+//mongodb://tygrys123:tygrys123@ds235078.mlab.com:35078/tigerserv_nas
 app = express()
+
+//conect to DB use mongoose
+mongoose.connect('mongodb://tygrys123:tygrys123@ds235078.mlab.com:35078/tigerserv_nas',{useNewUrlParser: true},() => {
+    console.log('Connected To Database!')
+})
 
 app.get('/',(req,res)=> res.send('server start') )
 
